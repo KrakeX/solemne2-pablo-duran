f9d7de6e5657afc0930290c282e89b99babb33b9
Pablo Durán Celis <duran.pablo.c@gmail.com>
2025-12-28 01:59:53 -0300

    se agregan endpoints de evaluations

--- backend/src/modules/evaluations/evaluations.controller.ts
+++ backend/src/modules/evaluations/evaluations.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { EvaluationsService } from './evaluations.service';
+
+import { CreateEvaluationDto } from './dto/create-evaluation.dto';
 
 @Controller('evaluations')
-export class EvaluationsController {}
+export class EvaluationsController {
+  constructor(private readonly evaluationsService: EvaluationsService) {}
+
+  @Post()
+  create(@Body() dto: CreateEvaluationDto) {
+    return this.evaluationsService.create(dto);
+  }
+}
--- backend/src/modules/evaluations/evaluations.service.ts
+++ backend/src/modules/evaluations/evaluations.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
+
+import { Enrollment } from '../enrollments/enrollment.entity';
+
+import { Evaluation } from './evaluation.entity';
+
+import { CreateEvaluationDto } from './dto/create-evaluation.dto';
 
 @Injectable()
-export class EvaluationsService {}
+export class EvaluationsService {
+  constructor(
+    @InjectRepository(Evaluation) private readonly evalRepo: Repository<Evaluation>,
+    @InjectRepository(Enrollment) private readonly enrRepo: Repository<Enrollment>,
+  ) {}
+
+  async create(dto: CreateEvaluationDto) {
+    const enrollment = await this.enrRepo.findOne({
+      where: { id: dto.enrollmentId },
+      relations: { evaluation: true },
+    });
+
+    if (!enrollment) throw new NotFoundException('Enrollment no encontrado');
+    if (enrollment.evaluation) throw new ConflictException('Este curso ya fue evaluado');
+
+    const evaluation = this.evalRepo.create({
+      enrollment,
+      score: dto.score,
+      comment: dto.comment.trim(),
+    });
+
+    return this.evalRepo.save(evaluation);
+  }
+}
