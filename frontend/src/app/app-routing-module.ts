38ca0628ee872a14e4e51814116e2db389ec77fc
Pablo Durán Celis <duran.pablo.c@gmail.com>
2025-12-28 00:35:59 -0300

    se agrega rutas y corrige el path hacia la pagina de errores

--- frontend/src/app/app-routing-module.ts
+++ frontend/src/app/app-routing-module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NotFoundComponent } from './pages/not-found/not-found';
+
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'evaluation' },
+
+  {
+    path: 'evaluation',
+    loadChildren: () =>
+      import('./modules/evaluation/evaluation-module').then((m) => m.EvaluationModule),
+  },
+  {
+    path: 'teachers',
+    loadChildren: () =>
+      import('./modules/teachers/teachers-module').then((m) => m.TeachersModule),
+  },
+  {
+    path: 'reports',
+    loadChildren: () =>
+      import('./modules/reports/reports-module').then((m) => m.ReportsModule),
+  },
+  {
+    path: 'history',
+    loadChildren: () =>
+      import('./modules/history/history-module').then((m) => m.HistoryModule),
+  },
+
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
