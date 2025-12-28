75ff72cdff1f33755f318a996d0ffe51c9c45247
Pablo Durán Celis <duran.pablo.c@gmail.com>
2025-12-28 02:15:12 -0300

    se agrega endpoint de consulta de carreras

--- backend/src/modules/careers/careers.module.ts
+++ backend/src/modules/careers/careers.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Career } from './career.entity';
+import { CareersController } from './careers.controller';
+import { CareersService } from './carreers.service';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Career])],
+    controllers: [CareersController],
+    providers: [CareersService],
 })
 export class CareersModule {}
