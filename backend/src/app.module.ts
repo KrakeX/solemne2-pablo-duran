5cd39cee7a9c1292649aee833fac13b34ecea4c9
Pablo Durán Celis <duran.pablo.c@gmail.com>
2025-12-28 01:27:26 -0300

    se crean los módulos necesarios para la API

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CareersModule } from './modules/careers/careers.module';
+import { StudentsModule } from './modules/students/students.module';
+import { TeachersModule } from './modules/teachers/teachers.module';
+import { CoursesModule } from './modules/courses/courses.module';
+import { PeriodsModule } from './modules/periods/periods.module';
+import { EnrollmentsModule } from './modules/enrollments/enrollments.module';
+import { EvaluationsModule } from './modules/evaluations/evaluations.module';
+import { ReportsModule } from './modules/reports/reports.module';
 
 @Module({
-  imports: [],
+  imports: [CareersModule, StudentsModule, TeachersModule, CoursesModule, PeriodsModule, EnrollmentsModule, EvaluationsModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
